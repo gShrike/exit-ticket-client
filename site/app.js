3edb695d717ecd8274fcfb1d7d5c964a93e05b90
coloradokim <schlesinger.kim@gmail.com>
2017-09-21 11:12:35 -0600

    add javascript

--- site/app.js
+++ site/app.js
@@ -1,1 +1,26 @@
-console.log("It's working");
+document.body.onload = getData;
+
+const QUESTION_URL = 'https://exit-ticketer.herokuapp.com/v1/api/questions';
+
+var orderedList = document.getElementsByTagName('ol');
+var listItem = document.createElement('li');
+
+function getData() {
+  fetch(QUESTION_URL)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(questions) {
+      
+      for (var i = 0; i < questions.length; i++) {
+        listItem.innerHTML = questions[i].question_text
+        orderedList[0].appendChild(listItem)
+      }
+    })
+};
+
+function createListItem() {
+
+}
+
+createListItem()
--- tests/show-questions.js
+++ tests/show-questions.js
@@ -4,7 +4,11 @@ module.exports = {
         .url("http://localhost:8080/")
         .waitForElementVisible("h1", 1000)
         .assert.containsText("h1", "Exit Ticketer")
-        
+        .waitForElementVisible("ol", 1000)
+        .waitForElementVisible("li", 6000)
+        .assert.containsText("li", "Describe agile velocity")
+        .assert.containsText("li", "Explain how you use git to create a feature branch workflow")
+        .assert.containsText("li", "What is one thing you learned today?")
         .end()
     }
 }
