3d0751b7115e7bea846671df8a4bee1b5b56fc75
coloradokim <schlesinger.kim@gmail.com>
2017-09-21 11:23:23 -0600

    add list items

--- site/app.js
+++ site/app.js
@@ -1,26 +1,19 @@
 document.body.onload = getData;
 
-const QUESTION_URL = 'https://exit-ticketer.herokuapp.com/v1/api/questions';
+const QUESTIONS_URL = 'https://exit-ticketer.herokuapp.com/v1/api/questions';
 
 var orderedList = document.getElementsByTagName('ol');
 var listItem = document.createElement('li');
 
 function getData() {
-  fetch(QUESTION_URL)
+  fetch(QUESTIONS_URL)
     .then(function(response) {
       return response.json()
     })
     .then(function(questions) {
-      
       for (var i = 0; i < questions.length; i++) {
         listItem.innerHTML = questions[i].question_text
         orderedList[0].appendChild(listItem)
       }
     })
 };
-
-function createListItem() {
-
-}
-
-createListItem()
--- tests/show-questions.js
+++ tests/show-questions.js
@@ -4,11 +4,9 @@ module.exports = {
         .url("http://localhost:8080/")
         .waitForElementVisible("h1", 1000)
         .assert.containsText("h1", "Exit Ticketer")
-        .waitForElementVisible("ol", 1000)
-        .waitForElementVisible("li", 6000)
-        .assert.containsText("li", "Describe agile velocity")
-        .assert.containsText("li", "Explain how you use git to create a feature branch workflow")
-        .assert.containsText("li", "What is one thing you learned today?")
+        .assert.containsText("li:nth-child(1)", "Describe agile velocity")
+        .assert.containsText("li:nth-child(2)", "Explain how you use git to create a feature branch workflow")
+        .assert.containsText("li:nth-child(3)", "What is one thing you learned today?")
         .end()
     }
 }
